00a1874b4f32cc07570708e3d67169a012a5bfda
tryptz <xboxps3456felix@gmail.com>
2026-08-01 11:23:14 -0400

    release: bump to 1.8.2 (versionCode 182)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,8 +52,8 @@ android {
         applicationId = "tf.monotrypt.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 181
-        versionName = "1.8.1"
+        versionCode = 182
+        versionName = "1.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
